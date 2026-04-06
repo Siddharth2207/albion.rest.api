fe7d7472dd887f9e220ffe7cd7e6406f977d3760
Alastair Ong <alastair.ong.a7o@gmail.com>
2026-04-06 14:50:15 +0300

    Migrate production host to 178.62.61.40

--- keys.nix
+++ keys.nix
@@ -5,7 +5,7 @@ rec {
     github_actions_deploy =
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN8tXytd8vWClKbJ+xSyCFNHlIaR4R4KGOb9IUGaxSlk";
     host =
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICMHgH+tMB9/MWcKQU5U07I02eMZmPrVvY8oSphg/1lp";
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIVjE3PzTWUFhP+f0gZMQ5/7rFktsUa4xNPd0co4IAks";
   };
 
   roles = with keys; {
